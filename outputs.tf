6be05f50702e8dbc115faf9b4b235bcf5240768f
seiji <toyama.seiji@gmail.com>
2019-12-04 18:54:25 +0900

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,21 @@ locals {
   value = jsondecode(data.aws_ssm_parameter.this.value)
 }
 
+output value {
+  value = local.value
+}
+
 output schema_version {
   value = local.value["schema_version"]
 }
 
+output block_device_mappings {
+  value = [
+    {
+      device_name = "/dev/xvda"
+    },
+  ]
+}
 output image_name {
   value = local.value["image_name"]
 }
@@ -14,7 +25,15 @@ output image_id {
   value = local.value["image_id"]
 }
 
+output id {
+  value = local.value["image_id"]
+}
+
 output os {
+  value = local.value["os"]
+}
+
+output architecture {
   value = local.value["os"]
 }
 
